f315b5897d486dfda38b2081b80ee60988f15506
jpceia <joao.p.ceia@gmail.com>
2018-10-08 23:54:24 +0100

    updated main.py and pyxcel.py

--- pyxcel/main.py
+++ pyxcel/main.py
@@ -1,5 +1,5 @@
 import pyxcel as pyx
-from flask import Flask
+from flask import Flask, request
 import inspect
 import json
 
@@ -8,30 +8,22 @@ import test
 app = Flask(__name__)
 
 
-@app.route("/query/<string:foo>/<string:args>")
-def query(foo, args):
-    assert foo in pyx.udf_dico
-    args = json.loads(args)
-    ret = json.dumps(pyx.udf_dico[foo](*args))
-    return ret
+@app.route("/eval", methods=["POST"])
+def eval():
+    query = request.json
+    foo_name = query["foo"]
+    args = query.get("args", ())
+    return json.dumps(pyx.eval(foo_name, *args))
 
 
-@app.route("/list_udf/")
-def list_udf():
-    dico = dict()
-    for name, foo in pyx.udf_dico.items():
-        detail = dict()
-        detail['args'] = inspect.getargspec(foo).args
-        if foo.__doc__ is not None:
-            detail['doc'] = foo.__doc__
-        dico[name] = detail
-    return json.dumps(dico)
+@app.route("/udf_signatures")
+def udf_signatures():
+    return json.dumps(pyx.udf_signatures())
 
 
 @app.route("/echo/<string:message>")
 def echo(message):
     return message
-
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
