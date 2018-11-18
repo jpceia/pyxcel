3323658a33b92e55c07b6777a381d9fe1a44f4a1
jpceia <joao.p.ceia@gmail.com>
2018-11-18 22:02:32 +0000

    changes to parse DocString and export relevant information to vba client

--- pyxcel/pyxcel.py
+++ pyxcel/pyxcel.py
@@ -1,3 +1,5 @@
+from docstring_parser import parse
+from collections import OrderedDict
 import inspect
 import sys
 
@@ -22,13 +24,43 @@ def import_module(dirname, filename):
     del sys.path[0]
 
 
+class DocString:
+    def __init__(self, foo):
+        doc = parse(getattr(foo, "__doc__", ""))
+        self.short_desc = doc.short_description
+        self.long_desc = doc.long_description
+        if doc.returns:
+            self.return_type = doc.returns.type_name
+        else:
+            self.return_type = None
+        argDico = dict()
+        for p in doc.params:
+            arg_row = dict()
+            if p.description:
+                arg_row['desc'] = p.description
+            if p.type_name:
+                arg_row['type'] = p.type_name
+            argDico[p.arg_name] = arg_row
+
+        self.args = []
+        for arg in inspect.getargspec(foo).args:
+            arg_row = dict()
+            arg_row['name'] = arg
+            if arg in argDico:
+                arg_row.update(argDico[arg])
+            self.args.append(arg_row)
+
 def signatures():
     dico = dict()
     for name, foo in udf_dico.items():
+        doc = DocString(foo)
         detail = dict()
-        detail['args'] = inspect.getargspec(foo).args
-        if foo.__doc__ is not None:
-            detail['doc'] = foo.__doc__
+        if doc.short_desc:
+            detail['doc'] = doc.short_desc
+        # detail['class'] = None
+        if doc.return_type:
+            detail['type'] = doc.return_type
+        detail['args'] = doc.args
         dico[name] = detail
     return dico
 
--- pyxcel/test.py
+++ pyxcel/test.py
@@ -1,10 +1,35 @@
 from pyxcel import export
+import random
+import numpy as np
 
 @export
-def add(x, y):
-    """ function to add two numbers """
+def add(y, x):
+    """
+    function to add two numbers
+    Returns x + y
+
+    Long description
+    :param double y: First argument to be added
+    :param double x: Second argument to be added
+    :return double: returns x + y
+    """
     return x + y
 
 @export
 def subtract(x, y):
     return x - y
+
+@export
+def reshape(x, n, m):
+    x = np.array(x).flatten().tolist()
+    res = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(x[i * n + j])
+        res.append(row)
+    return res
+
+@export
+def pyrandom():
+    return random.random()
