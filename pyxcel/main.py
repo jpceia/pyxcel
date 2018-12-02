32c5c817557687f19bedf55a2398720fcabcffd3
jpceia <joao.p.ceia@gmail.com>
2018-12-02 14:22:20 +0000

    model Capturing class to pyxcel.py

--- pyxcel/main.py
+++ pyxcel/main.py
@@ -6,32 +6,20 @@ try:
 except ImportError:
     from io import StringIO
 
-#import test
-
 app = Flask(__name__)
-
-
-class Capturing(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
 
 
 @app.route("/import", methods=["POST"])
 def import_files():
     query = request.json
     xldir = query['dir']
-    files = query['files']
-    for file in files:
-        if not os.path.isabs(file):
-            file = xldir + "\\" + file
-        pyx.import_module(file)
-    return "ok"
+    file = query['file']
+    try:
+        pyx.import_module(xldir, file)
+        return '1'
+    except ValueError as e:
+        print(e)
+        return '0'
 
 
 @app.route("/eval", methods=["POST"])
@@ -51,12 +39,11 @@ def signatures():
 def echo(message):
     return message
 
+
 @app.route("/execute", methods=["POST"])
 def execute():
-    s = request.json["command"]
-    with Capturing() as output:
-        exec(s)
-    return jsonify(output)
+    commands = request.json['command']
+    return jsonify(pyx.execute(commands))
 
 
 if __name__ == "__main__":
--- pyxcel/pyxcel.py
+++ pyxcel/pyxcel.py
@@ -1,33 +1,17 @@
 from docstring_parser import parse
 from collections import OrderedDict
+from importlib import reload
+import pandas as pd
+import numpy as np
 import inspect
 import os, sys
 
-udf_dico = dict()
+UDF_DICO = dict()
 VBA_NAMES = [
     "function",
     "sub",
     "next",
 ]
-
-def export(foo):
-    foo_name = foo.__name__
-    #assert foo_name not in udf_dico
-    assert foo_name not in VBA_NAMES
-    udf_dico[foo.__name__] = foo
-    return foo
-
-
-def import_module(path):
-    if not os.path.exists(path):
-        raise "{} doesn't exist".format(path)
-    folder, file = os.path.split(path)
-    mod, e = os.path.splitext(file)
-    if e != ".py":
-        raise "invalid file extension - needs to be .py"
-    sys.path.insert(0, folder)
-    __import__(mod)
-    del sys.path[0]
 
 
 class DocString:
@@ -56,14 +40,48 @@ class DocString:
                 arg_row.update(argDico[arg])
             self.args.append(arg_row)
 
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+
+def export(foo):
+    foo_name = foo.__name__
+    #assert foo_name not in udf_dico
+    assert foo_name not in VBA_NAMES
+    UDF_DICO[foo_name] = foo
+    return foo
+
+def import_module(root, file):
+    if not os.path.isabs(file):
+        file = root + "\\" + file
+    if not os.path.exists(file):
+        raise ValueError("{} doesn't exist".format(file))
+    folder, file = os.path.split(file)
+    mod, e = os.path.splitext(file)
+    if e != ".py":
+        raise ValueError("{} has an invalid file extension".format(file))
+    sys.path.insert(0, folder)
+    m = __import__(mod)
+    reload(m)
+    sys.path.pop(0)
+
+
 def signatures():
     dico = dict()
-    for name, foo in udf_dico.items():
+    for name, foo in UDF_DICO.items():
         doc = DocString(foo)
         detail = dict()
         if doc.short_desc:
             detail['doc'] = doc.short_desc
-        # detail['class'] = None
+        # detail['category'] = None
         if doc.return_type:
             detail['type'] = doc.return_type
         detail['args'] = doc.args
@@ -72,6 +90,24 @@ def signatures():
 
 
 def eval(foo_name, *args):
-    assert foo_name in udf_dico
-    foo = udf_dico[foo_name]
-    return foo(*args)
+    assert foo_name in UDF_DICO
+    foo = UDF_DICO[foo_name]
+    res = foo(*args)
+
+    if isinstance(res, pd.Series):
+        res = np.column_stack((res.index, res)).to_list()
+    elif isinstance(res, pd.DataFrame):
+        res = np.row_stack((
+            np.concatenate(([res.index.name], res.columns)),
+            np.column_stack((res.index, res)))).to_list()
+    elif isinstance(res, np.ndarray):
+        res = res.tolist()
+
+    return res
+
+
+def execute(commands):
+    s = request.json["command"]
+    with Capturing() as output:
+        exec(s)
+    return output
